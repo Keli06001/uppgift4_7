4a68c9e6864b1fc885f3e0de5fe70ea1ebb307f4
Kevin <Kevin@DESKTOP-27L22BB>
2023-10-22 12:44:07 +0200

    Add project files.

---
+++ uppgift4_7/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace uppgift4_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in ett meddelande");
+            string meddelande = Console.ReadLine();
+            Console.WriteLine("hur många steg åt höger vill du skriva ditt meddelande?");
+            int steg = int.Parse(Console.ReadLine());
+            for (int i = 0; i < meddelande.Length; i++) 
+            {
+                string bokstav = meddelande[i].ToString();
+                for (int j = 0; j < steg; j++)
+                {
+                    Console.Write(" ");
+                }
+                Console.WriteLine(bokstav);
+            }
+            Console.ReadKey();
+        }
+    }
+}
